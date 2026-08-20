3741f39d041a6a552cf8f23e7b72429465a07efe
Rasmika Billa <rasmikab2000@gmail.com>
2026-08-20 05:19:41 -0400

    day-40

---
+++ medium/39.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        ret = []
+        self.dfs(candidates, target, [], ret)
+        return ret
+
+    def dfs(self, nums, target, path, ret):
+        if target < 0:
+            return
+        if target == 0:
+            ret.append(path)
+            return
+        for i in range(len(nums)):
+            self.dfs(nums[i:], target-nums[i], path+[nums[i]], ret)
+        
